25a0b600827b80a4c9e27a630cbf32b20bb70a6c
M. Revan Apriyandi <revanapriyandi88@gmail.com>
2026-02-07 05:05:44 +0700

    fix: Ensure deployment directory exists before cd

--- resources/views/ssh/os/run-script.blade.php
+++ resources/views/ssh/os/run-script.blade.php
@@ -2,6 +2,8 @@
 # echo "Current directory: $(pwd)"
 # echo "Attempting to cd into: {{ $path }}"
 
+mkdir -p {{ $path }}
+
 if ! cd {{ $path }}; then
     echo "FAILED_TO_CD: Could not change directory to {{ $path }}"
     echo 'VITO_SSH_ERROR' && exit 1
